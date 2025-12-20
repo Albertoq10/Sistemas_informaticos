79f47abe858e501d90613ab2cb651e74b50e42d8
Josue M. Aguilar <josmiguel92@gmail.com>
2025-12-20 13:19:25 +0100

    clave de beto en el ejemplo para no romperte el environment

--- env.example.h
+++ env.example.h
@@ -3,6 +3,6 @@
 // Copia este archivo a env.h y configura tus valores.
 // No incluir env.h en el git.
 
-#define WIFI_SSID "YOUR_WIFI_SSID"
-#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
-#define SERVER_BASE_URL "http://<server-ip>:<port>"
+#define WIFI_SSID "iPhoneBETO"
+#define WIFI_PASSWORD "12345678"
+#define SERVER_BASE_URL "http://172.20.10.3:6000"
